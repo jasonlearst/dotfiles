c23f9d1c1f704d97fb5425089620c1d3f59fe88d
BigJay517 <bigjay517@gmail.com>
2015-09-30 16:29:51 -0400

    Added more multiplatform support to bash_profile.  source bash_profile in bashrc

--- install_unix.sh
+++ install_unix.sh
@@ -6,9 +6,9 @@
 
 ########## Variables
 
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vimrc vim bash_profile"    # list of files/folders to symlink in homedir
+dir=~/dotfiles                           # dotfiles directory
+olddir=~/dotfiles_old                    # old dotfiles backup directory
+files="vimrc vim bashrc bash_profile"    # list of files/folders to symlink in homedir
 
 ##########
 
