157518e0cdf50b07a816b8eded728ecf46105434
BigJay517 <bigjay517@gmail.com>
2016-03-01 22:52:27 -0500

    expect dotfiles to be checked out to .dotfiles

--- install_unix.sh
+++ install_unix.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 ############################
 # .make.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+# This script creates symlinks from the home directory to any desired dotfiles in ~/.dotfiles
 ############################
 
 ########## Variables
 
-dir=~/dotfiles                           # dotfiles directory
-olddir=~/dotfiles_old                    # old dotfiles backup directory
+dir=~/.dotfiles                           # dotfiles directory
+olddir=~/.dotfiles_old                    # old dotfiles backup directory
 files="vimrc vim bashrc bash_profile"    # list of files/folders to symlink in homedir
 
 ##########
@@ -25,7 +25,7 @@ echo "...done"
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 echo "Moving any existing dotfiles from ~ to $olddir"
 for file in $files; do
-    mv ~/.$file ~/dotfiles_old/
+    mv ~/.$file $olddir
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
