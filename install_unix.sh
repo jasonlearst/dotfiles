33eaa9506c02797dd6a94c785dcd1392def05f37
BigJay517 <bigjay517@gmail.com>
2016-03-25 15:29:06 -0400

    Adding .gitconfig.local for get user and email setup
    Inspired and modified from https://github.com/holman/dotfiles/

--- install_unix.sh
+++ install_unix.sh
@@ -3,6 +3,48 @@
 # .make.sh
 # This script creates symlinks from the home directory to any desired dotfiles in ~/.dotfiles
 ############################
+
+############################
+# Helper functions
+# https://github.com/holman/dotfiles/
+############################
+info () {
+   printf "\r  [ \033[00;34m..\033[0m ] $1\n"
+}
+
+user () {
+   printf "\r  [ \033[0;33m??\033[0m ] $1\n"
+}
+
+success () {
+   printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+}
+
+fail () {
+   printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
+   echo ''
+   exit
+}
+
+setup_gitconfig () {
+   if ! [ -f ~/.gitconfig.local ]
+   then
+      info 'setup gitconfig'
+
+      user ' - What is your github author name?'
+      read -e git_authorname
+      user ' - What is your github author email?'
+      read -e git_authoremail
+
+      sed -e "s/AUTHORNAME/$git_authorname/g" -e "s/AUTHOREMAIL/$git_authoremail/g" extras/.gitconfig.local.example > ~/.gitconfig.local
+
+      success 'gitconfig'
+   fi
+}
+
+############################
+# Main script
+############################
 # check the platfrom
 platform='unknown'
 unamestr=$(uname)
@@ -65,6 +107,9 @@ done
 for link in $links; do
    stow $link
 done
+
+# Run local gitconfig setup
+setup_gitconfig
 
 # install vim plugins using vim-plug
 echo "Installing vim plugins..."
